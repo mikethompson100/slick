00066ab8514ebd9b9309c69310f702dbca1062f6
Michael Thompson <mikethompson100@hotmail.com>
2022-12-25 14:38:06 -0500

    Ran site through HTML validator. Added accessibility styles to carousel dots and arrow buttons. Added dots to carousel 2.

--- js/main.js
+++ js/main.js
@@ -6,16 +6,25 @@ function setupImages() { // add same group of image nodes to each carousel
   for (let i = 1;  i < numOfCarousels+1; i++) {
       let string = "carousel" + i;
       var container = document.getElementById(string);
-       for (var j = 1; j < imagesTotal; j++) {
+       for (var j = 1; j <= imagesTotal; j++) {
           var node = new Image();
           node.src = 'img/0' + j + '.jpg';
+          switch (j) {
+            case 1: node.alt = "A photo of the city Zahara de la Sierra in Spain."; break;
+            case 2: node.alt = "A photo of a building in Malaga, Spain." + j; break;
+            case 3: node.alt = "A photo of a bull statue in the city of Ronda, Spain." + j; break;
+            case 4: node.alt = "A photo of gardens in the city of Granada, Spain." + j; break;
+            case 5: node.alt = "A photo of houses in Ronda, Spain." + j; break;
+            case 6: node.alt = "A photo of the city of Granada, Spain." + j; break;
+          }
+          
           container.appendChild(node);
         } 
   }
 }
 
 function initCarousels(numOfCarousels) { // run all slick carousels
-  for (let i = 1;  i < numOfCarousels+1; i++) {
+  for (let i = 1;  i <= numOfCarousels; i++) {
         node = '.carousel' + i;
           $(node).slick({});
   }
